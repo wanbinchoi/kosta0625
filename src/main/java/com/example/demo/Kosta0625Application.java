5b485954d17b2849940de262505e805ab08fbb7f
wanbinchoi <cwb6278@gmail.com>
2025-06-25 09:26:55 +0900

    first commit

---
+++ src/main/java/com/example/demo/Kosta0625Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosta0625Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosta0625Application.class, args);
+	}
+
+}
